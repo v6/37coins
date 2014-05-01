e9f0f5798c08ffcb0d7f8411772c7052595509d3
Johann Barbie <extraterrestrialintelligence@gmail.com>
2014-04-30 18:04:45 -0700

    fixed account button

--- webui/app/scripts/views/indexLayout.js
+++ webui/app/scripts/views/indexLayout.js
@@ -6,13 +6,21 @@ define([
 function(Backbone, Communicator, IndexLayout) {
     'use strict';
     return Backbone.Marionette.Layout.extend({
-	regions: {
-	    commands: '#smsCommands',
-	    gateways: '#gwTable'
-	},
-	template: IndexLayout,
-	className: 'main',
-	initialize: function() {
-	}
+        regions: {
+            commands: '#smsCommands',
+            gateways: '#gwTable'
+        },
+        template: IndexLayout,
+        className: 'main',
+        initialize: function() {
+        },
+        events: {
+            'click #smsSignup': 'handleJoin',
+        },
+        handleJoin: function(e){
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, 'slow');
+            $('[name="search"]').focus();
+        }
     });
 });
