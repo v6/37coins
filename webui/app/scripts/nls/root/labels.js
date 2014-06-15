309c53683d08d56f272deec6b05216d6ad19e6df
Johann Barbie <extraterrestrialintelligence@gmail.com>
2014-06-15 00:47:20 -0700

    adjusted links and labeled about page

--- webui/app/scripts/nls/root/labels.js
+++ webui/app/scripts/nls/root/labels.js
@@ -1,4 +1,11 @@
 define({
-  'key1' : 'This is key 1.',
-  'key2' : 'This is key 2.'
+    'aboutView' : {
+        'about' : 'About 37coins',
+        'desc' : 'We are committed to making bitcoin easy, secure to use, and accessible to everyone. With a global focus on social equity, we develop down-teched, secure, scalable solutions that are as appropriate for emerging economies as they are for developed ones.',
+        'team' : 'Team',
+        'role1' : 'Social Entrepreneur',
+        'role2' : 'Jack-of-all-trades Intern',
+        'role3' : 'Developer',
+        'role4' : 'Designer'
+    }
 });
