0a7e350bc72090bf370d3218bb849c6ade7815db
Nathan Basanese <nathan@basanese.com>
2014-07-03 15:17:19 -0700

    UNTESTED add labels for logout.
    P.S. Let me know if you want the next Nathan thing to be an addition of a logout page.

--- webui/app/scripts/views/logoutView.js
+++ webui/app/scripts/views/logoutView.js
@@ -1,7 +1,19 @@
-define(['backbone', 'communicator', 'hbs!tmpl/logoutView_tmpl'], function(Backbone, Communicator, LogoutTmpl) {
+define([
+    'backbone',
+    'communicator',
+    'hbs!tmpl/logoutView_tmpl',
+        'i18n!nls/labels',
+        'i18n!nls/webLabels'
+    ],
+
+    function(Backbone, Communicator, LogoutTmpl, myLabels, myWebLabels) {
     'use strict';
     return Backbone.Marionette.ItemView.extend({
         template: LogoutTmpl,
+        templateHelpers: function(){
+            console.log ( "Loading template helpers for logout" ) ; // DEBUGGING CODE
+            return window.helpers(myLabels, webLabels);
+        },
         className: 'container',
         initialize: function() {
             
